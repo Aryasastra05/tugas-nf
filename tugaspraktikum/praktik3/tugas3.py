596d3253bdcdb82c100d84d9c64eb15f7ee9d61e
Aryasastra05 <100260344+Aryasastra05@users.noreply.github.com>
2023-10-18 12:05:00 +0700

    Add files via upload

---
+++ tugaspraktikum/praktik3/tugas3.py
@@ -0,0 +1,47 @@
+nama = "arya"
+nim = "0110223044"
+kelas = "TI01"
+telp = "085700957991"
+alamat = "jakarta"
+
+print ("nama", nama)
+print ("nim", nim)
+print ("kelas", kelas)
+print ("telp", telp)
+print ("alamat", alamat)
+
+nama = "kahfa"
+nim = "0110223012"
+kelas = "TI01"
+telp = "08967780124"
+alamat = "depok"
+
+print ("nama", nama)
+print ("nim", nim)
+print ("kelas", kelas)
+print ("telp", telp)
+print ("alamat", alamat)
+
+tinggi_badan = 175
+berat_badan = (tinggi_badan - 100) - ((tinggi_badan - 100) * 0.1)
+
+print(berat_badan)
+
+celcius = 100
+fahreinheit = ((celcius * 9/5) + 32)
+
+print(fahreinheit)
+
+p = 22 / 7
+r = 6
+t = 10
+
+luas  = 2 *p * r * (r + t)
+print (int(luas))
+
+p = 22 / 7
+r = 8
+t = 10
+
+keliling = 2 * p * r
+print (int(keliling))
